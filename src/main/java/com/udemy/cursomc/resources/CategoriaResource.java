4f0f8b446cb837afba17687d8886ea5365d5cb55
Gabriel Aparecido <contato.gabrielaparecido36@gmail.com>
2020-09-09 12:16:04 -0300

    Criando repository e service para Categoria

--- src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
@@ -1,30 +1,29 @@
 package com.udemy.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@GetMapping
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.addAll(Arrays.asList(cat1, cat2));
-		
-		return lista;
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
+
+		Categoria obj = categoriaService.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
+	
 	
 }
---
+++ src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.udemy.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElse(null);
+	}
+
+	
+}
