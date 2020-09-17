56391d419080f943c8166706ae1c97322c9e690b
Gabriel Aparecido <contato.gabrielaparecido36@gmail.com>
2020-09-16 22:47:13 -0300

    Endpoint /clients/{id} disponível

---
+++ src/main/java/com/udemy/cursomc/resources/ClienteResource.java
@@ -0,0 +1,37 @@
+package com.udemy.cursomc.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.cursomc.domain.Cliente;
+import com.udemy.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService clienteService;
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Cliente>> findAll(){
+		List<Cliente> lista = clienteService.findAll();
+		return ResponseEntity.ok().body(lista);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Cliente> findById(@PathVariable Integer id) {
+		Cliente obj = clienteService.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+}
---
+++ src/main/java/com/udemy/cursomc/services/ClienteService.java
@@ -0,0 +1,30 @@
+package com.udemy.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.cursomc.domain.Cliente;
+import com.udemy.cursomc.repositories.ClienteRepository;
+import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não enconstrado! Id: "+ id + ", Tipo: "+ Cliente.class.getName()));
+	}
+	
+	public List<Cliente> findAll() {
+		List<Cliente> obj = clienteRepository.findAll();
+		return obj;
+	}
+
+	
+}
