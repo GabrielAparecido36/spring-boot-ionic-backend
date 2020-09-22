45853308bc37b20ebe2432345e9030b3dd152838
Gabriel Aparecido <contato.gabrielaparecido36@gmail.com>
2020-09-22 13:54:32 -0300

    Validação customizada: Email nõ repetido na inserção do Cliente

--- src/main/java/com/udemy/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/udemy/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.udemy.cursomc.domain.Cliente;
 import com.udemy.cursomc.domain.enums.TipoCliente;
 import com.udemy.cursomc.dto.ClienteNewDTO;
+import com.udemy.cursomc.repositories.ClienteRepository;
 import com.udemy.cursomc.resources.exceptions.FieldMessage;
 import com.udemy.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -29,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
