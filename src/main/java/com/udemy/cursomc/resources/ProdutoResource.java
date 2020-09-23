a901e0b986b29587dc593f7f1cc89be5d79326ab
Gabriel Aparecido <contato.gabrielaparecido36@gmail.com>
2020-09-22 22:51:19 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/udemy/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/udemy/cursomc/resources/ProdutoResource.java
@@ -3,34 +3,46 @@ package com.udemy.cursomc.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.cursomc.domain.Produto;
+import com.udemy.cursomc.dto.ProdutoDTO;
+import com.udemy.cursomc.resources.utils.URL;
 import com.udemy.cursomc.services.ProdutoService;
 
 @RestController
-@RequestMapping(value="/produtos")
+@RequestMapping(value = "/produtos")
 public class ProdutoResource {
 
 	@Autowired
 	private ProdutoService produtoService;
-	
-	@GetMapping
-	public ResponseEntity<List<Produto>> findAll(){
-		List<Produto> lista = produtoService.findAll();
-		return ResponseEntity.ok().body(lista);
-		
-	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Produto> findById(@PathVariable Integer id) {
 		Produto obj = produtoService.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
+
+	@GetMapping
+	public ResponseEntity<Page<ProdutoDTO>> findPage(@RequestParam(value = "nome", defaultValue = "") String nome,
+			@RequestParam(value = "categorias", defaultValue = "") String categorias,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+		String nomeDeCoded = URL.decodeParam(nome);
+		List<Integer> ids = URL.decodeIntList(categorias);
+		Page<Produto> lista = produtoService.search(nomeDeCoded, ids, page, linesPerPage, orderBy, direction);
+		Page<ProdutoDTO> listDto = lista.map(obj -> new ProdutoDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+
+	}
+
 }
