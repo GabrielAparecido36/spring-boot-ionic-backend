75dc5197e9e5b40b521fac790fd27ee93515ca95
Gabriel Aparecido <OUR_NAME@example.comcd>
2020-11-09 20:56:33 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/udemy/cursomc/resources/PedidoResource.java
+++ src/main/java/com/udemy/cursomc/resources/PedidoResource.java
@@ -6,16 +6,20 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.udemy.cursomc.domain.Categoria;
 import com.udemy.cursomc.domain.Pedido;
+import com.udemy.cursomc.dto.CategoriaDTO;
 import com.udemy.cursomc.services.PedidoService;
 
 @RestController
@@ -25,12 +29,12 @@ public class PedidoResource {
 	@Autowired
 	private PedidoService pedidoService;
 	
-	@GetMapping
-	public ResponseEntity<List<Pedido>> findAll(){
-		List<Pedido> lista = pedidoService.findAll();
-		return ResponseEntity.ok().body(lista);
-		
-	}
+//	@GetMapping
+//	public ResponseEntity<List<Pedido>> findAll(){
+//		List<Pedido> lista = pedidoService.findAll();
+//		return ResponseEntity.ok().body(lista);
+//		
+//	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Pedido> findById(@PathVariable Integer id) {
@@ -44,6 +48,17 @@ public class PedidoResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@GetMapping()
+	public ResponseEntity<Page<Pedido>> findPage(
+			@RequestParam(value="page", defaultValue = "0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue = "instante") String orderBy,
+			@RequestParam(value="direction", defaultValue = "DESC") String direction){
+		Page<Pedido> lista = pedidoService.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(lista);
+		
 	}
 	
 	
--- src/main/java/com/udemy/cursomc/services/PedidoService.java
+++ src/main/java/com/udemy/cursomc/services/PedidoService.java
@@ -5,8 +5,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.udemy.cursomc.domain.Cliente;
 import com.udemy.cursomc.domain.ItemPedido;
 import com.udemy.cursomc.domain.PagamentoComBoleto;
 import com.udemy.cursomc.domain.Pedido;
@@ -14,65 +18,76 @@ import com.udemy.cursomc.domain.enums.EstadoPagamento;
 import com.udemy.cursomc.repositories.ItemPedidoRepository;
 import com.udemy.cursomc.repositories.PagamentoRepository;
 import com.udemy.cursomc.repositories.PedidoRepository;
+import com.udemy.cursomc.security.UserSS;
+import com.udemy.cursomc.services.exceptions.AuthorizationException;
 
 @Service
 public class PedidoService {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ProdutoService produtoService;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private EmailService emailService;
-	
+
 	public Pedido buscar(Integer id) {
 		Optional<Pedido> obj = pedidoRepository.findById(id);
 		return obj.orElse(null);
 	}
-	
+
 	public List<Pedido> findAll() {
 		List<Pedido> obj = pedidoRepository.findAll();
 		return obj;
 	}
-	
+
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
 		obj.setCliente(clienteService.findById(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDETE);
 		obj.getPagamento().setPedido(obj);
-		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
 		obj = pedidoRepository.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-		
-		for(ItemPedido ip : obj.getItens()) {
+
+		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setProduto(produtoService.findById(ip.getProduto().getId()));
 			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		//emailService.sendOrderConfirmationHtmlEmail(obj);
-		//emailService.sendOrderConfirmationEmail(obj);
+		// emailService.sendOrderConfirmationHtmlEmail(obj);
+		// emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
 
-	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.findById(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
+	}
+
 }
